012724e23503484f3e2ee46d6b8c6c3609dc9a68
Meriem <meriem-chaieb@live.fr>
2021-07-08 18:00:34 +0100

    08/07/2021-01

--- src/app/casestudies/casestudies.component.ts
+++ src/app/casestudies/casestudies.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-casestudies',
   templateUrl: './casestudies.component.html',
-  styleUrls: ['./casestudies.component.css']
+  styleUrls: ['./casestudies.component.scss']
 })
 export class CasestudiesComponent implements OnInit {
 
